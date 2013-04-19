9839442cedd7f58f28bbcea796b485d6a2769017
andisimo <andrew.b.schultz@gmail.com>
2013-04-19 10:33:58 -0600

    BootstrapContext added

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
+using System.IdentityModel;
+using System.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.JWT;
 
 namespace TestACSApp.Controllers
 {
@@ -20,18 +24,21 @@ namespace TestACSApp.Controllers
         protected virtual string ExtractBootstrapToken()
         {
             //return HttpContext.Current.User.BootstrapToken();
+            BootstrapContext bc = ClaimsPrincipal.Current.Identities.First().BootstrapContext as BootstrapContext;
+            var jwt = bc.SecurityToken as JWTSecurityToken;
+            return jwt.ToString();
 
-            string token = null;
+            //string token = null;
 
-            // Substract the token from the bearer authorization header
-            IEnumerable<string> authzHeaders;
-            if (!Request.Headers.TryGetValues("Authorization", out authzHeaders) || authzHeaders.Count() > 1)
-            {
-                return "there was no auth header or more than 1. AuthController.ExtractBootstrapToken";
-            }
-            var bearerToken = authzHeaders.ElementAt(0);
-            token = bearerToken.StartsWith("Bearer ") ? bearerToken.Substring(7) : bearerToken;
-            return token;
+            //// Substract the token from the bearer authorization header
+            //IEnumerable<string> authzHeaders;
+            //if (!Request.Headers.TryGetValues("Authorization", out authzHeaders) || authzHeaders.Count() > 1)
+            //{
+            //    return "there was no auth header or more than 1. AuthController.ExtractBootstrapToken";
+            //}
+            //var bearerToken = authzHeaders.ElementAt(0);
+            //token = bearerToken.StartsWith("Bearer ") ? bearerToken.Substring(7) : bearerToken;
+            //return token;
         }
 
         public void Get()
